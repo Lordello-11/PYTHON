13b68719cae4cf0d23a767a840a2b46028970e26
Lordello-11 <rafaelmartinscardoso87@gmail.com>
2025-08-06 09:12:29 -0300

    no

---
+++ meus treinos/Treino2.py
@@ -0,0 +1,27 @@
+n1 = int(input(' escreva um numero:'))
+n2 = int(input('escreva outro numero:'))
+
+sub = n1 - n2
+som = n1 + n2
+mult = n1 * n2
+div = n1 / n2
+
+respostas = [sub,som,mult,div]
+
+print(respostas)
+
+op = respostas.index(sub)
+print(op)
+
+ 
+    
+
+
+
+
+
+
+
+
+
+
